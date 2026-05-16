7a8f87509f180c9e6402279c81ffc96cbce82af9
emjanka <eaa25emja@students.eaaa.dk>
2026-05-16 13:48:58 +0200

    start of the map

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./Views/Home";
 import LineUp from "./Views/LineUp";
+import Map from "./Views/Map";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="lineup" element={<LineUp />} />    
+        <Route path="lineup" element={<LineUp />} />
+        <Route path="map" element={<Map />} />    
       </Route>
     </Routes>
   );
