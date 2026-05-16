a20c39f50cc2743dc5ec6b19bd9a3e1a3a621896
MadlenGeorgieva <madlengeorgieva2005@gmail.com>
2026-05-16 12:51:15 +0200

    Fixed header and Footer and Scroll to Top when page loaded

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,20 +1,28 @@
 import { Outlet } from "react-router-dom";
+
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 
 import styles from "./Layout.module.css";
+import ScrollToTop from "./Components/ScrollToTop";
 
 function Layout() {
   return (
     <div className={styles.layout}>
-      <Header />
+  <ScrollToTop />
 
-      <main className={styles.content}>
-        <Outlet />
-      </main>
+  <div className={styles.headerFixed}>
+    <Header />
+  </div>
 
-      <Footer />
-    </div>
+  <main className={styles.content}>
+    <Outlet />
+  </main>
+
+  <div className={styles.footerFixed}>
+    <Footer />
+  </div>
+</div>
   );
 }
 
