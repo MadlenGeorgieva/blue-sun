15adc54a1af2a30137c8cb783d197907a62cd784
MadlenGeorgieva <madlengeorgieva2005@gmail.com>
2026-05-18 11:41:42 +0200

    v11

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,21 +13,31 @@ function Header({
 }) {
   const navigate = useNavigate();
 
+  const handleLeftClick = () => {
+    if (onLeftClick) {
+      onLeftClick();
+    } else {
+      navigate("/notifications");
+    }
+  };
+
+  const handleRightClick = () => {
+    if (onRightClick) {
+      onRightClick();
+    } else {
+      navigate("/profile");
+    }
+  };
+
   return (
     <header className={styles.header}>
-      <button
-        className={styles.iconButton}
-        onClick={onLeftClick}
-      >
+      <button className={styles.iconButton} onClick={handleLeftClick}>
         <img src={leftIcon} alt="" />
       </button>
 
       <img className={styles.logo} src={logo} alt="Blå Sol" />
 
-      <button
-        className={styles.iconButton}
-        onClick={onRightClick}
-      >
+      <button className={styles.iconButton} onClick={handleRightClick}>
         <img src={rightIcon} alt="" />
       </button>
     </header>
