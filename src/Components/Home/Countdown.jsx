142308abfa32c1e004f28143c83a349a0e09ff0b
MadlenGeorgieva <madlengeorgieva2005@gmail.com>
2026-05-21 12:19:10 +0200

    comments 2

--- src/Components/Home/Countdown.jsx
+++ src/Components/Home/Countdown.jsx
@@ -1,8 +1,11 @@
 import styles from "./Countdown.module.css";
 import halfSun from "../../assets/HalfSun.png";
 
+// Countdown component displayed on the home page
+// Shows the number of days left until the festival together with date and location information
 function Countdown() {
   return (
+
     <section className={styles.countdown}>
       <div className={styles.text}>
         <h1>50</h1>
